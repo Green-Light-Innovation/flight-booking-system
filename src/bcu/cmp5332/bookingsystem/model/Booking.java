7b5cb1a355f406be0acc1d321792e8205b7c94de
J4CKINS <electronicjack4@gmail.com>
2022-12-06 12:21:26 +0000

    Implemented booking model

--- src/bcu/cmp5332/bookingsystem/model/Booking.java
+++ src/bcu/cmp5332/bookingsystem/model/Booking.java
@@ -2,18 +2,61 @@ package bcu.cmp5332.bookingsystem.model;
 
 import java.time.LocalDate;
 
+/**
+ * Class model that represents a booking made in the system
+ * @author Jack Atkins &amp; Daniel Jukes
+ */
 public class Booking {
     
     private Customer customer;
     private Flight flight;
     private LocalDate bookingDate;
     
-
+    /**
+     * @param customer as {@code Customer}
+     * @param flight {@code Flight}
+     * @param bookingDate {@code LocalDate}
+     */
     public Booking(Customer customer, Flight flight, LocalDate bookingDate) {
-        // TODO: implementation here
-        
+        this.customer = customer;
+        this.flight = flight;
+        this.bookingDate = bookingDate;
     }
     
-    // TODO: implementation of Getter and Setter methods
+    // Getters
     
+    /**
+     * @return the customer that made the booking
+     */
+    public Customer getCustomer() { return customer; }
+    
+    /**
+     * @return the flight that the booking was made for
+     */
+    public Flight getFlight() { return flight; }
+    
+    /**
+     * @return the date that the booking was made
+     */
+    public LocalDate getBookingDate() { return bookingDate; }
+    
+    // Setters
+    
+    /**
+     * Set the customer who created the booking
+     * @param customer as {@code Customer}
+     */
+    public void setCustomer(Customer customer) { this.customer = customer; }
+    
+    /**
+     * Set the flight that the booking is for
+     * @param flight as {@code Flight}
+     */
+    public void setFlight(Flight flight) { this.flight = flight; }
+    
+    /**
+     * Set the date that the booking was made
+     * @param as {@code LocalDate}
+     */
+    public void setBookingDate(LocalDate date) { this.bookingDate = date; }
 }
