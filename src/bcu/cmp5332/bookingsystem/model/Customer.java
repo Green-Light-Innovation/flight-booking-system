ba7c961409f636ca4b906f678b8fedbecb94dd16
J4CKINS <electronicjack4@gmail.com>
2022-12-06 21:15:18 +0000

    Implemented Customer model

--- src/bcu/cmp5332/bookingsystem/model/Customer.java
+++ src/bcu/cmp5332/bookingsystem/model/Customer.java
@@ -4,6 +4,11 @@ import bcu.cmp5332.bookingsystem.main.FlightBookingSystemException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Class model that represents a customer in the system
+ * @author Jack Atkins
+ * @author Daniel Jukes
+ */
 public class Customer {
     
     private int id;
@@ -11,11 +16,115 @@ public class Customer {
     private String phone;
     private final List<Booking> bookings = new ArrayList<>();
     
-    // TODO: implement constructor here
+    /**
+     * @param id
+     * @param name
+     * @param phone
+     */
+    public Customer(int id, String name, String phone) {
+    	this.id		= id;
+    	this.name	= name;
+    	this.phone	= phone;
+    }
     
-    // TODO: implementation of Getter and Setter methods
+    // GETTERS
+    /**
+     * @return the customer's ID
+     */
+    public int getID() { return id; }
     
-    public void addBooking(Booking booking) {
-        // TODO: implementation here
+    /**
+     * @return the customer's name
+     */
+    public String getName() { return name; }
+    
+    /**
+     * @return the customer's phone number
+     */
+    public String getPhone() { return phone; }
+    
+    /**
+     * @return A list of the customers bookings
+     */
+    public List<Booking> getBookings() { return bookings; }
+    
+    /**
+     * @return basic customer details
+     */
+    public String getDetailsShort() {
+    	return "Id: " + id + " - " + name + " - " + phone;
+    }
+    
+    /**
+     * @return customer's details and bookings
+     */
+    public String getDetailsLong() {
+    	String details = "Customer #" + id + "\n" +
+    			"Name: " + name + "\n" +
+    			"Phone: " + phone + "\n" +
+    			"--------------------------\n" +
+    			"Bookings:\n";
+    	
+    	for(Booking booking : bookings) {
+    		String bookingDate = booking.getBookingDate().toString();
+    		Flight flight = booking.getFlight();
+    		details += "* Booking date: " +  bookingDate + " for " + flight.getDetailsShort() + "\n";
+    	}
+    	details += bookings.size() + " booking(s)";
+    	
+    	return details;
+    }
+    
+    // SETTERS
+    /**
+     * Set the customer's ID
+     * @param id as {@code int}
+     */
+    public void setID(int id) { this.id = id; }
+    
+    /**
+     * Set the customer's name
+     * @param name as {@code String}
+     */
+    public void setName(String name) { this.name = name; }
+    
+    /**
+     * Set the customer's phone number
+     * @param phone as {@code String}
+     */
+    public void setPhone(String phone) { this.phone = phone; }
+    
+    /**
+     * Adds a booking object to the customer's list of bookings
+     * @throws {@code bcu.cmp5332.bookingsystem.main.FlightBookingSystemException} if the customer's list of bookings already contains the specified booking}
+     * @param booking as {@code Booking}
+     */
+    public void addBooking(Booking booking) throws FlightBookingSystemException {
+    	
+    	for(Booking customerBooking : bookings) {
+    		// Compare both flight ID to check if they are the same flight
+    		if (booking.getFlight().getId() == customerBooking.getFlight().getId()) {
+    			throw new FlightBookingSystemException("This flight is already booked");
+    		}
+    	}
+    	
+    	bookings.add(booking);
+    }
+    
+    /**
+     * Removes the specified booking from the customer's list of bookings
+     * @throws {@code bcu.cmp5332.bookingsystem.main.FlightBookingSystemException} if the customer’s list of bookings doesn’t contain a booking for the flight that is given as a parameter
+     * @param flight as {@code Flight}
+     */
+    public void cancelBooking(Flight flight) throws FlightBookingSystemException {
+    	
+    	for (Booking booking : bookings) {
+    		if (booking.getFlight().getId() == flight.getId()) {
+    			bookings.remove(booking);
+    			return;
+    		}
+    	}
+    	
+    	throw new FlightBookingSystemException("Could not find the specified flight to cancel");
     }
 }
---
+++ src/bcu/cmp5332/bookingsystem/test/ModelTest.java
@@ -0,0 +1,67 @@
+package bcu.cmp5332.bookingsystem.test;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+import org.junit.Test;
+import org.junit.Assert;
+import bcu.cmp5332.bookingsystem.model.*;
+import bcu.cmp5332.bookingsystem.main.FlightBookingSystemException;
+
+public class ModelTest {
+	
+	Customer customer;
+	Flight flight;
+	Booking booking;
+	FlightBookingSystem system;
+	
+	// Customer Model Tests
+	@Test
+	public void testNewCustomer() {
+		customer = new Customer(1, "John Doe", "07777777777");
+		Assert.assertEquals(1, customer.getID());
+		Assert.assertEquals("John Doe", customer.getName());
+		Assert.assertEquals("07777777777", customer.getPhone());
+		Assert.assertEquals(Collections.EMPTY_LIST, customer.getBookings());
+	}
+	
+	@Test
+	public void testCustomerAddBooking() throws FlightBookingSystemException {
+		// Test that a booking can be created and added to the customer's booking list
+		customer = new Customer(1, "John Doe", "07777777777");
+		flight = new Flight(1, "Flight1", "UK", "USA", LocalDate.parse("2022-11-11"));
+		booking = new Booking(customer, flight, LocalDate.parse("2022-11-11"));
+		
+		customer.addBooking(booking);
+		
+		Assert.assertEquals(1, customer.getBookings().size());
+		
+		// Test that a booking with the same flight can't be made
+		Assert.assertThrows(FlightBookingSystemException.class, () -> { customer.addBooking(booking); });
+		
+		// Test that another booking with a different flight can be made
+		flight = new Flight(2, "Flight2", "UK", "USA", LocalDate.parse("2022-12-11"));
+		booking = new Booking(customer, flight, LocalDate.parse("2022-11-11"));
+		
+		customer.addBooking(booking);
+		
+		Assert.assertEquals(2, customer.getBookings().size());
+	}
+	
+	@Test
+	public void testCustomerCancelBooking() throws FlightBookingSystemException {
+		// Test that an already existing booking can be cancelled
+		customer = new Customer(1, "John Doe", "07777777777");
+		flight = new Flight(1, "Flight1", "UK", "USA", LocalDate.parse("2022-11-11"));
+		booking = new Booking(customer, flight, LocalDate.parse("2022-11-11"));
+		customer.addBooking(booking);
+		
+		customer.cancelBooking(flight);
+		
+		Assert.assertEquals(Collections.EMPTY_LIST, customer.getBookings());
+		
+		// Test that a booking can't be cancelled if it doesn't exist
+		Assert.assertThrows(FlightBookingSystemException.class, () -> { customer.cancelBooking(flight); });
+	}
+ }
